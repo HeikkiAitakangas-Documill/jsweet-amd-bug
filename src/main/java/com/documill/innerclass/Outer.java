ab7396a7890f91cce738b58f29b768186a990814
Heikki Aitakangas <heikki.aitakangas@documill.com>
2016-10-07 13:08:55 +0300

    A second bug: code generation for AMD modules sometimes fails on inner classes

---
+++ src/main/java/com/documill/innerclass/Outer.java
@@ -0,0 +1,8 @@
+package com.documill.innerclass;
+
+public class Outer
+{
+  public static class Inner
+  {
+  }
+}
---
+++ src/main/java/com/documill/innerclass/Problem.java
@@ -0,0 +1,11 @@
+package com.documill.innerclass;
+
+import com.documill.innerclass.Outer.Inner;
+
+public class Problem
+{
+  public static void problem()
+  {
+    Inner inner = new Inner();
+  }
+}
